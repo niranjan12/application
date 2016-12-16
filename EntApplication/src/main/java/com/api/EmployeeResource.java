bf82139ac2c4b9d2927c4ad8cb75b474b04b84e1
NiranjanSahu629 <NSahu-consultant@Scholastic.com>
2016-12-16 16:10:49 +0530

    dev:Updates

--- EntApplication/src/main/java/com/api/EmployeeResource.java
+++ EntApplication/src/main/java/com/api/EmployeeResource.java
@@ -1,7 +1,11 @@
 package com.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.EmployeeDao;
+import com.dto.Employee;
 
 
 
@@ -9,10 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeResource {
+	@Autowired
+	EmployeeDao dao;
 	
+	public EmployeeResource(){}
+	public EmployeeResource(EmployeeDao dao){
+		this.dao=dao;
+	}
 	@RequestMapping("/hello")
-	public String getEmployeeDetailsByName(){
-		return "Hello Niranjan...!!!";
+	public Employee getEmployeeDetailsByName(){
+		System.out.println(dao);
+		Employee e=dao.getEmployeeByName("Niranjan Sahu");
+		System.out.println(e.getName());
+		return e;
 	}
 
 }
